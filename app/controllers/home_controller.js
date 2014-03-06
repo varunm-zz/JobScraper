23e36c57c5dd300739321a42c2aec4b63ec35537
Alex Egan <aegan@andrew.cmu.edu>
2014-03-06 15:36:02 -0500

    changes variable names to match the paramater names in the API call

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -11,19 +11,19 @@ exports.search = function(req, res) {
 exports.login = function(req, res) {
 	var client_id = keys.api_key;
 	var state = keys.state;
-	var redirect_url = 'http://localhost:3000/return';
-	var auth_url = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id="+client_id+"&state="+state+"&redirect_uri="+redirect_url;
+	var redirect_uri = 'http://localhost:3000/return';
+	var auth_url = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id="+client_id+"&state="+state+"&redirect_uri="+redirect_uri;
 	return res.redirect(auth_url);
 };
 
 exports.return = function(req, res) {
-		var redirect_url = 'http://localhost:3000/return';
+		var redirect_uri = 'http://localhost:3000/return';
 		var client_id = keys.api_key;
 		var client_secret = keys.secret_key;
 		var code = req.param('code');
-		var post_url = "https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code="+code+"&redirect_uri="+redirect_url+"&client_id="+client_id+"&client_secret="+client_secret;
+		var post_url = "https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code="+code+"&redirect_uri="+redirect_uri+"&client_id="+client_id+"&client_secret="+client_secret;
 		// now I need to send that as a post request. I don't know how to make it work with the same URI required
-		// as the callback URI for both the above login function and this function. 
+		// as the callback URI for both the above login function and this function.
 };
 
 exports.post_return = function(req, res) {
