92cd445008bb7b9d61bdf5196d5a85991cc1fea7
Alex Egan <aegan@andrew.cmu.edu>
2014-03-06 00:34:22 -0500

    gutted most of the passport stuff to attemp to get it up to 2.0

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var http = require('http');
-var passport = require('passport');
 var util = require('util');
-var LinkedInStrategy = require('passport-linkedin').Strategy;
 var api_keys = require(__dirname + '/config/keys');
-
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.session({ secret: 'cmuis272' }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(app.router);
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'ejs');
@@ -22,43 +17,6 @@ app.use(express.methodOverride());
 // API keys and such
 var LINKEDIN_API_KEY = api_keys.api_key;
 var LINKEDIN_SECRET_KEY = api_keys.secret_key;
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new LinkedInStrategy({
-    consumerKey: LINKEDIN_API_KEY,
-    consumerSecret: LINKEDIN_SECRET_KEY,
-    callbackURL: "http://localhost:3000/auth/linkedin/callback"
-  },
-  function(token, tokenSecret, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      passport.session.token = token;
-      passport.session.secret = tokenSecret;
-      // To keep the example simple, the user's LinkedIn profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the LinkedIn account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
-));
-
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
-}
 
 console.log('listening on port 3000');
 app.listen(3000);
--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -1,19 +1,9 @@
 var request = require('request');
-var passport = require('passport');
 var keys = require('../../config/keys');
 exports.index = function(req, res) {
-	res.render('index', {user: req.user, 'token': passport.session.token, 'secret': passport.session.secret });
-}
+	res.render('index');
+};
 
 exports.search = function(req, res) {
-	var title = req.body.title;
-	var token = passport.session.token;
-	var key = keys.api_key;
-	request("http://api.linkedin.com/v1/people/~?format=json&oauth_consumer_key=" + key + "&oauth_nonce=1234&oauth_signature=68pmwRNPzWGnAAW6hUL3jvl48nw%3D&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1393998136&oauth_token="+ token +"&oauth_version=1.0", function(err, response, body) {
-		res.render('searchResults', {'title': title, 'body': body});
-	});
-}
-
-exports.test = function(req, res) {
-	res.render('test');
-}
+	res.render('searchResults', {'title': req.body.title});
+};
