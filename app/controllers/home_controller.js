95e17274442dd29fa9e5dce860e5f39852d64a34
Alex Egan <aegan@andrew.cmu.edu>
2014-03-06 00:56:53 -0500

    getting the correct response. Now for the tokens

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -7,3 +7,16 @@ exports.index = function(req, res) {
 exports.search = function(req, res) {
 	res.render('searchResults', {'title': req.body.title});
 };
+
+exports.login = function(req, res) {
+	var client_id = keys.api_key;
+	var state = keys.state;
+	var redirect_url = 'http://localhost:3000/return';
+	var auth_url = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id="+client_id+"&state="+state+"&redirect_uri="+redirect_url;
+	return res.redirect(auth_url);
+};
+
+exports.return = function(req, res) {
+	// I don't think this always works... I don't know why. But 4 in a row is enough for a commit
+	res.send("code is " + req.param('code') + "\n state is " + req.body.state);
+};
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,10 @@
 module.exports = function(app){
 	// controllers
 	var home = require('../app/controllers/home_controller');
-	
+
 	// routes
 	app.get('/', home.index);
 	app.post('/search', home.search);
+	app.get('/login', home.login);
+	app.get('/return', home.return);
 }
