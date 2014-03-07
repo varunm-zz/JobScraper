c36aafadd1ba9520ab516116f08bf81329ea2ef8
Alex Egan <aegan@andrew.cmu.edu>
2014-03-07 03:01:37 -0500

    this does not work but I am pushing for demonstration tomorrow

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -21,9 +21,36 @@ exports.return = function(req, res) {
 		var client_id = keys.api_key;
 		var client_secret = keys.secret_key;
 		var code = req.param('code');
-		var post_url = "https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code="+code+"&redirect_uri="+redirect_uri+"&client_id="+client_id+"&client_secret="+client_secret;
-		// now I need to send that as a post request. I don't know how to make it work with the same URI required
-		// as the callback URI for both the above login function and this function.
+		// this is driving me crazy. I don't understand how this is supposed to work.
+		// I am quite literally copying things from their website and trying it and it fails.
+		// client_id and secret are copied from the developer page. The redirect uri is specified on the developer page as well.
+		// the code they give me is the code I send back, and I copied the post request from the how to authenticate article and
+		// it does not work. I don't think there is any more I can do.
+
+		// here I just put every option in as a parameter
+		var post_uri = "https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=" + code + "&redirect_uri=" + redirect_uri + "&client_id=" + client_id + "&client_secret=" + client_secret;
+		// this gives me this error: {"error":"invalid_request","error_description":"missing required parameters, includes an invalid parameter value, parameter more then once. : Unable to retrieve access token : appId or redirect uri does not match authorization code or authorization code expired"}
+		// and here I try to put it in as a form body for a post request
+		// var post_uri = "https://www.linkedin.com/uas/oauth2/accessToken"
+		// request({
+		// 	uri: post_uri,
+		// 	method: 'POST',
+		// form: {
+		// 		grant_type: "authorization_code",
+		// 		code: code,
+		// 		redirect_uri: redirect_uri,
+		// 		client_id: client_id,
+		// 		client_secret: client_secret
+		//   },
+		// }, function(err, response, body) {
+		// 	if(err) {
+		// 		console.log("You got an error");
+		// 	}
+		// 	else {
+		// 		res.send(body);
+		// 	}
+		// });
+		// with this I get the error: {"error":"invalid_request","error_description":"missing required parameters, includes an invalid parameter value, parameter more then once. : Unable to retrieve access token : appId or redirect uri does not match authorization code or authorization code expired"}
 };
 
 exports.post_return = function(req, res) {
